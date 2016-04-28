c4c74a30f908d3d524a3aee4f67fa0f7571d9e3c
Paulo Henrique de Oliveira <pauloh2004@gmail.com>
2016-04-28 02:01:17 -0300

    Gerando expediente e os eventos.

--- ControleHoras/WebContent/WEB-INF/views/index.jsp
+++ ControleHoras/WebContent/WEB-INF/views/index.jsp
@@ -1,22 +1,22 @@
 <div style="margin:5px;background-color:white;">
 
-	<form method="POST" action="newoperation">
-		<input type="hidden" name="descricao" id="descricao" value="testealgumtestes"/>
+	<form method="POST" action="novaOperacao">
+		<input type="hidden" name="id" id="id_inicio_expediente" value="1"/>
 		<input type="submit" name="btn_inicio_exped" id="btn_inicio_exped" value="Inicio Expediente"/>
 	</form>
 	
-	<form method="POST" action="newoperation">
-		<input type="hidden" name="descricao" id="descricao" value="testealgumtestes2"/>
+	<form method="POST" action="novaOperacao">
+		<input type="hidden" name="descricao" id="id_" value="3"/>
 		<input type="submit" name="btn_inicio_almoco" id="btn_inicio_almoco" value="Inicio Almoço"/>
 	</form>
 	
-	<form method="POST" action="newoperation">
-		<input type="hidden" name="descricao" id="descricao" value="testealgumtestes3"/>
+	<form method="POST" action="novaOperacao">
+		<input type="hidden" name="id" id="id_fim_almoco" value="4"/>
 		<input type="submit" name="btn_fim_almoco" id="btn_fim_almoco" value="Fim Almoço"/>
 	</form>
 	
-	<form method="POST" action="newoperation">
-		<input type="hidden" name="descricao" id="descricao" value="testealgumtestes4"/>
+	<form method="POST" action="novaOperacao">
+		<input type="hidden" name="id" id="id_fim_exped" value="2"/>
 		<input type="submit" name="btn_fim_exped" id="btn_fim_exped" value="Fim Expediente"/>
 	</form>
 </div>
--- ControleHoras/src/br/com/paulo/controlehoras/controlers/ExpedienteControler.java
+++ ControleHoras/src/br/com/paulo/controlehoras/controlers/ExpedienteControler.java
@@ -1,14 +1,26 @@
 package br.com.paulo.controlehoras.controlers;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import br.com.paulo.controlehoras.dao.ExpedienteDAO;
 import br.com.paulo.controlehoras.dao.OperacaoDAO;
 import br.com.paulo.controlehoras.dao.TipoOperacaoDAO;
+import br.com.paulo.controlehoras.dao.UsersDAO;
+import br.com.paulo.controlehoras.dao.UsuarioDAO;
+import br.com.paulo.controlehoras.model.Expediente;
+import br.com.paulo.controlehoras.model.Operacao;
+import br.com.paulo.controlehoras.model.OperacaoPK;
 import br.com.paulo.controlehoras.model.TipoOperacao;
+import br.com.paulo.controlehoras.model.Usuario;
+import br.com.paulo.controlehoras.model.UsuarioPK;
 
 @Controller
 public class ExpedienteControler {
@@ -21,7 +33,14 @@ public class ExpedienteControler {
 	
 	@Autowired
 	private TipoOperacaoDAO tipoopDAO;
+	
+	@Autowired
+	private UsuarioDAO usuarioDAO;
 
+	@Autowired
+	private UsersDAO usersDAO;
+	
+	
 	public ExpedienteDAO getExpDAO() {
 		return expDAO;
 	}
@@ -43,14 +62,32 @@ public class ExpedienteControler {
 		return "index";
 	}
 	
-	@RequestMapping(value="/newoperation", method=RequestMethod.POST)
-	public String processRegistration(TipoOperacao tipoOperacao) {
-		//Se for inicio expediente, cria o expediente
-		//cria a operação
+	@RequestMapping(value="/novaOperacao", method=RequestMethod.POST)
+	public String processRegistration(Model model, TipoOperacao tipoOperacao) {
+		int id_expediente = 0;
+		if(tipoOperacao.getId()==1){
+			Usuario usuario = null;
+			Expediente expediente = new Expediente();
+			SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+			try {
+				usuario = usuarioDAO.getById(new UsuarioPK("31973777886", sdf.parse("1984-04-18")));
+				
+				expediente.setCpf_usuario(usuario);
+				expediente.setDataNascimento(sdf.parse("1984-04-18"));
+			} catch (ParseException e) {
+				e.printStackTrace();
+			}
+			
+			expDAO.save(expediente);
+			id_expediente=expediente.getId();
+		}
 		
-		//se não for inicio, cria a operação.
+		Operacao operacao = new Operacao(new OperacaoPK(tipoOperacao.getId(), id_expediente));
+		operacao.setData_hora(new Date());
 		
-		tipoopDAO.save(tipoOperacao);
-		return "redirect:/index/";
+		opDAO.save(operacao);
+		
+		//Deve retornar para a view o id do expediente.
+		return "redirect:/index";
 	}
 }
